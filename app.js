5081ec0c265d393c157986bf65953044435e5096
Peyton <prjunjulas@gmail.com>
2022-09-19 13:18:15 -0500

    final style changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs");
-
+app.use(express.static(path.join(__dirname, 'imgs')));
  
 let userName = "you can change this if you would like!";
 //let thatData = "";
